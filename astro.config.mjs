8a1007953d17929d1ecb05f1845b5f04617439b3
Cameron Jipp <cameronjipp@MacBook-Pro-2.local>
2025-10-24 13:36:22 -0400

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abrazodeoso.org',
+  site: 'https://camjipp.github.io',
+  base: '/abrazo-de-oso',
   compressHTML: true,
 });
 
